b61786d4417a8a594933b6965c29159f8c7ea17b
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-04-23 19:14:42 +0200

    Finish main chat functionality

--- server.js
+++ server.js
@@ -1,32 +1,44 @@
 var mongo = require('mongodb').MongoClient,
-    client = require('socket.io').listen(8080).sockets;
+    client = require('socket.io').listen(9000).sockets;
 
-mongo.connect('mongodb://127.0.0.1/NodeJS_Chat', function(err, db) {
+mongo.connect('mongodb://127.0.0.1/NodeJS_Chat', function(err, db){
     if(err) throw err;
 
     client.on('connection', function(socket) {
 
-        var col = db.collection('messages');
-
-            //For security reasons, important to take care of the status on the server side.
+        var col = db.collection('messages'),
+        //For security reasons, important to take care of the status on the server side.
             sendStatus = function(s) {
                 socket.emit('status', s);
             };
 
-        //Wait for input
-        socket.on('input', function(data) {
+        //Emit all messages
+        col.find().limit(100).sort({_id: 1}).toArray(function(err, res) {
+            if(err) throw err;
+            socket.emit('output', res);
+        });
 
+        //Wait for user input
+        socket.on('input', function(data) {
             var name = data.name,
                 message = data.message,
                 //Check for white spaces
                 whitespacePattern = /^\s*$/;
-            if (whitespacePattern.test(name) || whitespacePattern.test(message)) {
+
+            if(whitespacePattern.test(name) || whitespacePattern.test(message)) {
                 sendStatus('Name and message are required.');
             } else {
                 col.insert({name: name, message: message}, function() {
-                    console.log('Inserted');
+                    client.emit('output', [data]);
+
+                    sendStatus({
+                        message: "Message sent",
+                        clear: true
+                    });
                 });
             }
+
         });
+
     });
 });
