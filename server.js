f5b91413d43c894069c21ebe93b6a305d911b6bc
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-04-23 16:24:25 +0200

    Reinstall dependencies

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ mongo.connect('mongodb://127.0.0.1/NodeJS_Chat', function(err, db) {
 
         var col = db.collection('messages');
 
+            //For security reasons, important to take care of the status on the server side.
+            sendStatus = function(s) {
+                socket.emit('status', s);
+            };
+
         //Wait for input
         socket.on('input', function(data) {
 
@@ -16,7 +21,7 @@ mongo.connect('mongodb://127.0.0.1/NodeJS_Chat', function(err, db) {
                 //Check for white spaces
                 whitespacePattern = /^\s*$/;
             if (whitespacePattern.test(name) || whitespacePattern.test(message)) {
-                console.log('Invalid');
+                sendStatus('Name and message are required.');
             } else {
                 col.insert({name: name, message: message}, function() {
                     console.log('Inserted');
