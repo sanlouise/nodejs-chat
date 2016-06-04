0b31bd99cc1b4c6628a12a3606f5eea1050df39b
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-04 19:21:03 +0200

    Clean up code

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 var mongo = require('mongodb').MongoClient,
     client = require('socket.io').listen(9000).sockets;
 
-mongo.connect('mongodb://127.0.0.1/NodeJS_Chat', function(err, db){
-    if(err) throw err;
+mongo.connect('mongodb://127.0.0.1/NodeJS_Chat', function(err, db) {
+    if (err) throw err;
 
     client.on('connection', function(socket) {
 
         var col = db.collection('messages'),
-        //For security reasons, important to take care of the status on the server side.
+            //For security reasons, important to take care of the status on the server side.
             sendStatus = function(s) {
                 socket.emit('status', s);
             };
 
         //Emit all messages
-        col.find().limit(100).sort({_id: 1}).toArray(function(err, res) {
-            if(err) throw err;
+        col.find().limit(100).sort({
+            _id: 1
+        }).toArray(function(err, res) {
+            if (err) throw err;
             socket.emit('output', res);
         });
 
@@ -25,10 +27,13 @@ mongo.connect('mongodb://127.0.0.1/NodeJS_Chat', function(err, db){
                 //Check for white spaces
                 whitespacePattern = /^\s*$/;
 
-            if(whitespacePattern.test(name) || whitespacePattern.test(message)) {
+            if (whitespacePattern.test(name) || whitespacePattern.test(message)) {
                 sendStatus('Name and message are required.');
             } else {
-                col.insert({name: name, message: message}, function() {
+                col.insert({
+                    name: name,
+                    message: message
+                }, function() {
                     client.emit('output', [data]);
 
                     sendStatus({
